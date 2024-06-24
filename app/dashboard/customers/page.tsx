d808e0ad9591a3a23ca8674783c1d7afab4a0bd9
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-06-24 18:27:34 -0300

    adicionar auteticaçoa

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,17 @@
+// 'use client';
+
+// import { useEffect } from 'react';
+
 export default function Page() {
+  // useEffect(() => {
+  //   const fetchTodos = async () => {
+  //     const res = await fetch('http://localhost:3000/dashboard/customers/api');
+
+  //     const data = await res.json();
+  //     console.log({ data });
+  //   };
+
+  //   fetchTodos();
+  // }, []);
   return <p>Customers Page</p>;
 }
